1f1e211cc8a830cb04bc21f0c3a95157da97a8b4
Trevor Smith <trevorsmith772@u.boisestate.edu>
2021-02-17 21:12:25 -0700

    Added DFAState Constructor

--- fa/dfa/DFAState.java
+++ fa/dfa/DFAState.java
@@ -4,4 +4,11 @@ import fa.State;
 
 public class DFAState extends State {
     
+    /* Instance Variables */
+    private DFAState[] transitions;
+
+    public DFAState(String name){
+        this.name = name;
+        transitions = new DFAState[2];
+    }
 }
